ccc9762306b247f8a6a92d58733a9e85faf351c2
ogtay <ogtayhuseynov0@gmail.com>
2020-05-29 14:01:13 +0400

    link cards

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -2,13 +2,25 @@
 	export let segment;
 </script>
 
-<nav>
-	<ul>
-		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
-		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
-	</ul>
-</nav>
+<ul class="flex flex-col w-full font-medium">
+				<li class="flex mx-2 mb-1">
+					<a class:active-link={segment === undefined} 
+					class=" flex text-center w-full block rounded py-2 px-4 text-gray-200 hover:text-green-600" href="/">
+					<svg xmlns="http://www.w3.org/2000/svg"
+					 width="24" height="24"
+					  fill="none"
+					   stroke="currentColor" 
+					   stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+					    class=" feather feather-check-circle"><path d="M22 11.08V12a10 10 0 11-5.93-9.14"/><path d="M22 4L12 14.01l-3-3"/></svg>
+						<span class="ml-6">Complete</span>
+					</a>
+				</li>
+				<li class="flex mx-2">
+					<a class:active-link={segment === 'underdev'}
+					 class=" flex text-center w-full block rounded py-2 px-4 text-gray-200 hover:text-green-600"
+					 href="/underdev">
+					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-code"><path d="M16 18l6-6-6-6M8 6l-6 6 6 6"/></svg>
+						<span class="ml-6">Under Dev</span>
+					</a>
+				</li>
+			</ul>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,7 +1,7 @@
 <script>
-	// import Nav from '../components/Nav.svelte';
+	import Nav from '../components/Nav.svelte';
 
-	// export let segment;
+	export let segment;
 </script>
 <!-- <Nav {segment}/> -->
 <!-- 
@@ -17,26 +17,7 @@
 	</div>
 	<div class="flex h-screen">
 		<div class=" w-64 min-w-64 bg-gray-900 flex"> 
-			<ul class="flex flex-col w-full font-medium">
-				<li class="flex mx-2">
-					<a class=" flex text-center w-full block rounded py-2 px-4 text-gray-200 hover:bg-gray-800" href="/">
-					<svg xmlns="http://www.w3.org/2000/svg"
-					 width="24" height="24"
-					  fill="none"
-					   stroke="currentColor" 
-					   stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-					    class=" feather feather-check-circle"><path d="M22 11.08V12a10 10 0 11-5.93-9.14"/><path d="M22 4L12 14.01l-3-3"/></svg>
-						<span class="ml-6">Complete</span>
-					</a>
-				</li>
-				<li class="flex mx-2">
-					<a class=" flex text-center w-full block rounded py-2 px-4 text-gray-200 hover:bg-gray-800"
-					 href="/underdev">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-code"><path d="M16 18l6-6-6-6M8 6l-6 6 6 6"/></svg>
-						<span class="ml-6">Under Dev</span>
-					</a>
-				</li>
-			</ul>
+			<Nav segment={segment}></Nav>
 		</div>
 		<div class="flex-grow bg-gray-100 p-4">
 			<slot></slot>
